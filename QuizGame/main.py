9e6971c3e393671749e2d717a2ae32678c7d86de
rymyung <rymyung@hanmail.net>
2022-02-15 20:56:23 +0900

    [Update] Basic OOP

---
+++ QuizGame/main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = [Question(question['text'], question['answer']) for question in question_data]
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print('You\'ve completed the quiz!')
+print(f'Your final score was: {quiz.score}/{len(question_bank)}')
