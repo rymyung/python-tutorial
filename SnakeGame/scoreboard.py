b70990eabf73e63e1789b9ab38113a21bb2b2878
rymyung <rymyung@hanmail.net>
2022-02-22 20:20:13 +0900

    [Update] Record high score

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -9,7 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', 'r') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(x=0, y=270)
@@ -21,19 +22,15 @@ class ScoreBoard(Turtle):
         self.write(f'Score: {self.score} High Score: {self.high_score}', align=ALIGNMENT, font=FONT)
     
     
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
-        
-    
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', 'w') as data:
+                data.write(f'{self.high_score}')
         self.score = 0
         self.clear()
         self.update_scoreboard()
         
-    
     
     def increase_score(self):
         self.score += 1
